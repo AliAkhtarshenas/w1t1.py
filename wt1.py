9acfaf087cf41b7a2fd5503e742c7386c41bb0ed
AliAkhtarshenas <89966202+AliAkhtarshenas@users.noreply.github.com>
2022-02-07 18:50:45 +0330

    Add files via upload

---
+++ wt1.py
@@ -0,0 +1,499 @@
+# print("hello, world")
+
+# x = 5
+# y = "ali"
+# print(x)
+# print(y)
+
+# x = 4
+# x = "ali"
+# print("x")
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(x,y,z)
+
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"#
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+# 2myvar = "John"#
+# my-var = "John"#
+# my var = "John"#
+
+# myVariablename = "john"
+
+# my_variable_name = "john"
+
+# x, y, z = "orang", "banana", "cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x=y=z = "orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = "awsome"
+# print("python is :" + x)
+
+# x = "Python is "
+# y = "awesome"
+# z =  x + y
+# print(z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = 5#
+# y = "John"#
+# print(x + y)#
+
+# x = "awesome"
+# def myfunc() :
+#     print("python is " + x)
+# myfunc()
+
+# x = "awesome"
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+# myfunc()
+# print("Python is " + x)
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+# myfunc()
+# print("Python is " + x)
+
+# x = "awesome"
+# def myfunce():
+#     global x
+#     x = "fantastic"
+# myfunce()
+# print("python is " + x)
+
+# x = 5
+# print(type(x))
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 1
+# y = 35656222554887711
+# z = -3255522
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 1.10
+# y = 1.0
+# z = -35.59
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 3+5j
+# y = 5j
+# z = -5j
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+# #convert from int to float:
+# a = float(x)
+# #convert from float to int:
+# b = int(y)
+# #convert from int to complex:
+# c = complex(x)
+# print(a)
+# print(b)
+# print(c)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# import random
+# print(random.randrange(1, 10))
+
+# x = int(1)
+# y = int(2.8)
+# z = int("3")
+
+# x = float(1)
+# y = float(2.8)
+# z = float("3")
+# w = float("4.2")
+
+# x = str("s1")
+# y = str(2)
+# z = str(3.0)
+
+# print("Hello")
+# print('Hello')
+
+# a = "Hello"
+# print(a)
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+# print(a)
+
+# a = "Hello, world"
+# print(a[1])
+
+# for x in "banana":
+#   print(x)
+
+# a = "Hello, world"
+# print(len(a))
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print("No, 'expensive' is NOT present.")
+
+# b = "Hello, world!"
+# print(b[2:5])
+
+# b = "Hello, World!"
+# print(b[:5])
+
+# b = "Hello, World!"
+# print(b[2:])
+
+# b = "Hello, World!"
+# print(b[-5:-2])
+
+# a = ("Hello, world!")
+# print(a.upper())
+
+# a = ("Hello, world!")
+# print(a.lower())
+
+# a = "Hello, World!"
+# print(a.strip())
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
+# a = "Hello, World!"
+# print(a.split(","))
+
+# a = "Hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+# a = "Hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+# age = 36
+# txt = "My name is John, I am " + age
+# print(txt)
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want {} pieces of item {} for {} dollars."
+# print(myorder.format(quantity, itemno, price))
+
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity, itemno, price))
+
+# txt = "We are the so-called "Vikings" from the north."#
+
+# txt = "We are the so-called \"Vikings\" from the north."
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+# print(bool("Hello"))
+# print(bool(15))
+
+# bool("abc")
+# bool(123)
+# bool(["apple", "cherry", "banana"])
+
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
+
+# class myclass():
+#   def __len__(self):
+#     return 0
+# myobj = myclass()
+# print(bool(myobj))
+
+# print(10 + 5)
+
+# mylist = ["apple", "banana", "cherry"]
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = ["abc", 34, True, 40, "male"]
+
+# list1 = ["abc", 34, True, 40, "male"]
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+# thislist = list(("apple", "banana", "cherry"))
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:4])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thistuple = ("kiwi", "orange")
+# thislist.extend(thistuple)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+# print(newlist)
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+# print(newlist)
+
+# newlist = [x for x in fruits if x != "apple"]
+
+# newlist = [x for x in fruits]
+
+# newlist = [x for x in range(10)]
+
+# newlist = [x for x in range(10) if x < 5]
+
+# newlist = [x.upper() for x in fruits] 
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# def myfunc(n):
+#   return abs(n - 50)
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.sort(key = str.lower)
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.reverse()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+# list3 = list1 + list2
+# print(list3) 
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+# for x in list2:
+#   list1.append(x)
+# print(list1) 
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+# list1.extend(list2)
+# print(list1)
+
